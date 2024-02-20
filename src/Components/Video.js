00b80c3882533f18df13cb01396251841852dad6
Anwar5533 <anwarkornipalli@gmail.com>
2024-02-21 00:19:48 +0530

    First commit

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,5 +1,5 @@
 
-import { useContext, useEffect } from 'react';
+import { useContext} from 'react';
 import './Video.css'
 import ThemeContext from '../Context/ThemeContext';
 // import VideoDispatchContext from '../Context/VideoDispatchContext';
